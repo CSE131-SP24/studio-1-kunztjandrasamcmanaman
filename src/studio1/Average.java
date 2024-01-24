d497dbe538aea90c925a6e6d556be79d0889c5f7
jordi <jordi@LAPTOP-9J9O60V0.dhcp.wustl.edu>
2024-01-24 14:15:34 -0600

    Studio 1

---
+++ src/studio1/Average.java
@@ -0,0 +1,21 @@
+package studio1;
+
+import java.util.Scanner;
+
+public class Average {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner in = new Scanner(System.in);
+		System.out.println("Value for n1?");
+		int n1 = in.nextInt();
+		System.out.println("Value for n2?");
+		int n2 = in.nextInt();
+		int add = n1 + n2;
+		double avg = add / 2.0;
+		System.out.println("The average of " + n1 + " and " + n2 + " is " + avg);
+		
+	}
+
+}
